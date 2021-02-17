3869ac9d4d74787598914700789b51e8beca36d3
Krithik Tamilvanan <krithiktamilvanan@Krithiks-Macbook-Pro.local>
2021-02-16 17:00:22 -0800

    Gradebook

---
+++ gradebook.py
@@ -0,0 +1,17 @@
+last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
+
+subjects = ["physics", "calculus", "poetry", "history"]
+
+grades = [98, 97, 85, 88]
+
+subjects.append("computer science")
+grades.append(100)
+
+subjects.append("visual arts")
+grades.append(93)
+
+gradebook = list(zip(subjects, grades))
+
+print(gradebook)
+
+full_gradebook = list(last_semester_gradebook + gradebook)
